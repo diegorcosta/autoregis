615bd17b470de89a0907e15a0c742d64437d9056
Diego Costa <dgrcosta@gmail.com>
2022-08-10 15:24:34 -0300

    veiculos

--- src/pages/Venda.js
+++ src/pages/Venda.js
@@ -42,6 +42,143 @@ const Empresa = () => {
                   <input type="tel" name="cell" required />
                 </div>
               </div>
+              <h3>Dados do veículo</h3>
+              <div className="flex-items">
+                <div className="item">
+                  <label for="brand">
+                    Marca <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="brand" required />
+                </div>
+                <div className="item">
+                  <label for="model">
+                    Modelo <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="model" required />
+                </div>
+                <div className="item">
+                  <label for="version">
+                    Versão <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="version" required />
+                </div>
+                <div className="item">
+                  <label for="color">
+                    Cor <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="color" required />
+                </div>
+                <div className="item">
+                  <label for="km">
+                    Km <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="km" required />
+                </div>
+                <div className="item">
+                  <label for="plate">
+                    Placa <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="plate" required />
+                </div>
+                <div className="item">
+                  <label for="year">
+                    Ano / Modelo <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="year" required />
+                </div>
+                <div className="item">
+                  <label for="accessories">
+                    Acessórios <span>(campo obrigatório)</span>
+                  </label>
+                  <input type="text" name="accessories" required />
+                </div>
+              </div>
+              <label for="first-owner">
+                Você é o primeiro proprietário? <span>(campo obrigatório)</span>
+              </label>
+              <div className="radio-input">
+                <div className="radio">
+                  <input
+                    id="owner-yes"
+                    type="radio"
+                    name="first-owner"
+                    required
+                  />
+                  <label for="owner-yes">Sim</label>
+                </div>
+                <div className="radio">
+                  <input
+                    id="owner-no"
+                    type="radio"
+                    name="first-owner"
+                    required
+                  />
+                  <label for="owner-no">Não</label>
+                </div>
+              </div>
+              <label for="dirt-stains">
+                O estofamento possui manchas? <span>(campo obrigatório)</span>
+              </label>
+              <div className="radio-input">
+                <div className="radio">
+                  <input
+                    id="stains-yes"
+                    type="radio"
+                    name="dirt-stains"
+                    required
+                  />
+                  <label for="stains-yes">Sim</label>
+                </div>
+                <div className="radio">
+                  <input
+                    id="stains-no"
+                    type="radio"
+                    name="dirt-stains"
+                    required
+                  />
+                  <label for="stains-no">Não</label>
+                </div>
+              </div>
+              <label for="insurance">
+                O veículo tem seguro? <span>(campo obrigatório)</span>
+              </label>
+              <div className="radio-input">
+                <div className="radio">
+                  <input
+                    id="insurance-yes"
+                    type="radio"
+                    name="insurance"
+                    required
+                  />
+                  <label for="insurance-yes">Sim</label>
+                </div>
+                <div className="radio">
+                  <input
+                    id="insurance-no"
+                    type="radio"
+                    name="insurance"
+                    required
+                  />
+                  <label for="insurance-no">Não</label>
+                </div>
+              </div>
+              <label for="stolen">
+                O veículo já foi recuperado de roubo?{" "}
+                <span>(campo obrigatório)</span>
+              </label>
+              <div className="radio-input">
+                <div className="radio">
+                  <input id="stolen-yes" type="radio" name="stolen" required />
+                  <label for="stolen-yes">Sim</label>
+                </div>
+                <div className="radio">
+                  <input id="stolen-no" type="radio" name="stolen" required />
+                  <label for="stolen-no">Não</label>
+                </div>
+              </div>
+              <button type="submit" name="submit">
+                Enviar
+              </button>
             </form>
           </div>
         </div>
