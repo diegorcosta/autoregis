013a614f5b42e17f167c938d8ebd26be7b82ee2f
Diego Costa <dgrcosta@gmail.com>
2023-01-23 22:02:35 -0300

    fix token

--- src/services/RequestAPI.js
+++ src/services/RequestAPI.js
@@ -11,7 +11,7 @@ import loadingSVG from "../images/loading.svg";
 const Request = (props) => {
   const url = "https://api-webmotors.sensedia.com/oauth/v1/access-token";
   const token =
-    "MTJjMWI3N2ItZjk1Mi0zZTVhLWFjNzktNGUwNDIyNGJhNTNjOjU1ZDI1YWQ3LWJiMGUtM2RlYy04N2NjLTRkZTNiMGY0MjU5NQ==";
+    "ZDMxOTZiMjEtNzYzOS0zMWZkLWExZGQtN2Q1YTcyZjUyYjNkOmU0OTZmZGVhLTEzOWEtMzI2Ny1iNGRjLTAzMmM3NzU3NGY5Yw==";
 
   const [vehicles, setVehicles] = useState([]);
   const [loading, setLoading] = useState(true);
